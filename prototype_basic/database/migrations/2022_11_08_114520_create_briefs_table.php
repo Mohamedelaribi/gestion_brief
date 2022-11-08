135b16347c51f7092ca539879b545d7be3237fc9
Mohamedelaribi <mohamed.elaribi.solicode@gmail.com>
2022-11-08 13:02:12 +0100

    push

---
+++ prototype_basic/database/migrations/2022_11_08_114520_create_briefs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('briefs', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('apprenant_id');
+            $table->string('nameBrief');
+            $table->string('detailBrief');
+            $table->date('starDate');
+            $table->date('endDate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('briefs');
+    }
+};
